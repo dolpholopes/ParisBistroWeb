7e68e2fadb3e14be909a87b41b72defe28f6909d
rodolpho <dolpholopes@gmail.com>
2021-04-13 23:48:08 -0300

    Painel V1.2

--- public/js/notificacao.js
+++ public/js/notificacao.js
@@ -1,62 +1,73 @@
-function validarCamposNotificação(){
-	const titulo = document.getElementById("tituloNotificacao").value
-	const mensagem = document.getElementById("mensagemNotificacao").value
+function validarCamposNotificacao(){
 
-	if(titulo.trim() == "" || mensagem.trim() == "" ){
-		abrirModalAlerta("Preencha todos os campos")
-	}
-	else{
-		abrirModalProgress()
-		obterDadosNotificacao(titulo,mensagem)
-	}
+    const titulo = document.getElementById("tituloNotificacao").value
+    const mensagem = document.getElementById("mensagemNotificacao").value
+
+    if(titulo.trim() == "" || mensagem.trim() == ""){
+
+        abrirModalAlerta("Preencha os campos obrigatórios")
+    }else{
+
+        obterDadosNotificacao(titulo,mensagem)
+    }
 }
 
 function obterDadosNotificacao(titulo,mensagem){
     firebase.firestore().collection("app").doc("notificacao").get().then(function(documento){
+
         const dados = documento.data()
+
         const key = dados.key
         const topico = dados.topico
 
         abrirModalProgress()
         post(titulo,mensagem,topico,key)
+
     }).catch(function(error){
+
         abrirModalAlerta("Erro ao enviar Notificação: "+ error)
     })
+}
+
+function post(titulo,mensagem,topico,key){
+
+    const xmlHttpRequest = new XMLHttpRequest()
+    const url = "https://fcm.googleapis.com/fcm/send"
+
+    xmlHttpRequest.open("POST",url,true)
+    xmlHttpRequest.setRequestHeader("Content-Type","application/json")
+    xmlHttpRequest.setRequestHeader("Authorization",key)
+
+    xmlHttpRequest.onreadystatechange = function(){
+
+        removerModalProgress()
+
+        if(xmlHttpRequest.status == 200){
+
+            limparCampos()
+            abrirModalAlerta("Sucesso ao enviar notificação - Alguns clientes vão receber dentro de 5 minutos")
+        }
+        else{
+            
+            abrirModalAlerta("Falha ao enviar Notificação")
+        }
+    }
+ 
+    const parametros = {
+        "to": topico,
+        "data": {
+            "titulo":titulo,
+            "mensagem":mensagem,
+            "url_imagem": "https://firebasestorage.googleapis.com/v0/b/painel-delivery.appspot.com/o/app%2Fhomeapp%2Fimagem2?alt=media&token=2c6f9488-2752-42cd-a4b8-dc3e5057cfc6"
+        }
+    }
+
+    const notificacao = JSON.stringify(parametros)
+
+    xmlHttpRequest.send(notificacao)
 
 }
 
-function post(titulo, mensagem, topico, key){
-	const xmlHttpRequest = new XMLHttpRequest()
-
-	const url = "https://fcm.googleapis.com/fcm/send"
-
-	xmlHttpRequest.open("POST", url, true)
-	xmlHttpRequest.setRequestHeader("Content-Type","application/json")
-	xmlHttpRequest.setRequestHeader("Authorization", key)
-
-	xmlHttpRequest.onreadystatechange = function(){
-		if(xmlHttpRequest.status == 200){
-			limparCampos()
-			abrirModalAlerta("Sucesso ao enviar a notificação")
-		}
-		else{
-			abrirModalAlerta("Erro ao enviar notificação")
-		}
-	}
-
-	const parametros = {
-		"to": topico,
-		"data":  {
-			"titulo":titulo,
-			"mensagem":mensagem,
-			"url_imagem": "https://firebasestorage.googleapis.com/v0/b/paris-bistro-6b97e.appspot.com/o/app%2Fhomeapp%2Flogo_branca.png?alt=media&token=7f741e31-3f7d-4731-9c9a-9cbabebb0f32"
-		}
-	}
-
-	const notificacao = JSON.stringify(parametros)
-
-	xmlHttpRequest.send(notificacao)
-}
 
 
 //==================================================== MODAL PROGRESSBAR ====================================================
